2baf0535829609001dfdd007342f12c1c42e3983
Guilherme Mendes <boralagui@outlook.com>
2024-03-10 02:06:57 -0300

    Commitando Implementação de metodo instalar telefone na classe Nokia e Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,54 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero) : base(numero)
+        {
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Boolean status = true;
+            Boolean statusMemoria = true;
+            
+            
+            if(status) {
+                ReceberLigacao();
+            }
+            else
+            {
+                status = false;
+            }
+
+            if (statusMemoria)
+            {
+                int memoria = 0;
+                GravarDados(memoria);
+            }
+            else
+            {
+                statusMemoria = false;
+            }
+        }
+
+        public void GravarDados(int memoria)
+        {   
+            if (memoria > 0) 
+            {
+                Console.WriteLine("Memoria Vazia");
+            }
+            else
+            {
+                Console.WriteLine("Memoria cheia");
+            }
+        }
+        
+        public void VerificaIMei()
+        {
+
+        }
     }
 }
